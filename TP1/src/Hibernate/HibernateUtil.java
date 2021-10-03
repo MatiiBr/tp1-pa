95a595dccdd3878e308032465f93eba640457583
ElianEnria <eliannben2@gmail.com>
2021-10-03 14:25:34 -0300

    ALGO

--- TP1/src/Hibernate/HibernateUtil.java
+++ TP1/src/Hibernate/HibernateUtil.java
@@ -11,29 +11,29 @@ import org.hibernate.cfg.*;
 public class HibernateUtil {
     public static SessionFactory sessionFactory;
     public static Session session;
-    
+
     public static void inicializar() {
         try {
            Configuration conf = new Configuration(); 
            try{
                 conf.setProperty("hibernate.connection.driver_class","org.postgresql.Driver");
-                conf.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");                 
+                conf.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");
                 conf.setProperty("hibernate.connection.url","jdbc:postgresql://localhost:5432/tp1");
             }
             catch(Exception e){
                 JOptionPane.showMessageDialog(null, "Error de Base de Datos Nº 2001");
             }
-            
-            conf.setProperty("hibernate.connection.username","postgres");
-            conf.setProperty("hibernate.connection.password","");
 
-            conf.setProperty("hibernate.connection.pool_size","10");                          
+            conf.setProperty("hibernate.connection.username","postgres");
+            conf.setProperty("hibernate.connection.password","1234");
+
+            conf.setProperty("hibernate.connection.pool_size","10");
             conf.setProperty("hibernate.hbm2ddl.auto","update");
 
             conf.addPackage("Modelos.Gestion");
             conf.addAnnotatedClass(Modelos.Gestion.Contacto.class);
             conf.addAnnotatedClass(Modelos.Gestion.Persona.class);
-            
+
             try {
                     sessionFactory = conf.buildSessionFactory();
                     session=sessionFactory.openSession();
@@ -47,7 +47,7 @@ public class HibernateUtil {
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+
     public static Session getSession()
     throws HibernateException {
         return session;
